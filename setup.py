aa1cafdf1efe16990cf24f8d4d68bdbb2aa1102b
happyleaves <happyleaves.tfr@gmail.com>
2015-11-29 15:33:11 -0500

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='orvibo',
-    version='1.0.0',
+    version='1.0.1',
     description='Control Orvibo products.',
     url='https://github.com/happyleavesaoc/python-orvibo/',
     license='MIT',
